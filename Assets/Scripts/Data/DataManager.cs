b2a42bd7b47a69c991532530ac8bd576c2482fce
caocahunyang <642381965@qq.com>
2018-03-29 22:15:01 +0800

    f

---
+++ Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using LitJson;
+
+public class DataManager
+{
+    public void Save(Object tobject)
+    {
+        string path = "";
+        string serializedString = JsonMapper.ToJson(tobject);
+        using (StreamWriter sw = File.CreateText(path))
+        {
+            sw.Write(serializedString);
+        }
+    }
+    public T Load<T>()
+    {
+        string path = "";
+        if (File.Exists(path) == false)
+        {
+            return default(T);
+        }
+
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string stringEncrypt = sr.ReadToEnd();
+
+            if (string.IsNullOrEmpty(stringEncrypt))
+                return default(T);
+            return JsonMapper.ToObject<T>(stringEncrypt);
+        }
+    }
+}
